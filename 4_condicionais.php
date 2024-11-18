e4a041d185f903c5bafcf689095fc25d2d310a9b
Yasmin Lopes Borba <yasmin.b0rbalopes@gmail.com>
2024-11-18 14:15:24 -0300

    resolução ex4

--- 4_condicionais.php
+++ 4_condicionais.php
@@ -1,7 +1,22 @@
 <?php
 
-// Digitar PHP (1º Aqui)
+// Digitar PHP (1º Aqui), realiza a verificação dos dados. Utilizando o metodo POST
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $senha = $_POST['senha'];
+
+    if($senha === '1234567'){
+        //redireciona para pagina de 'boas vindas'
+        header("Location:4b_bem_vindo.php");
+
+        exit();
+    }else{
+
+        //mensagem de erro
+        $erro = "Senha incorreta. Tente novamente";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -19,7 +34,13 @@
 
     <?php
 
-    // Digitar PHP (2º Aqui)
+    // Digitar PHP (2º Aqui), retorna o valor de acordo com a condicional 
+
+    //exibe mensagem de erro se existir 
+
+    if (isset($erro)){
+        echo "<p style='color: red;'>$erro</p>";
+    }
 
     ?>
 </body>
