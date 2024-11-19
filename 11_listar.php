95bb2545d34ad49158673cb95b66afd9eeca6725
Yasmin Lopes Borba <yasmin.b0rbalopes@gmail.com>
2024-11-19 10:45:28 -0300

    resolução ex11

--- 11_listar.php
+++ 11_listar.php
@@ -1,6 +1,6 @@
 <?php
 // Conecta ao banco de dados
-$servername = "localhost";
+$servername = "localhost:3309";
 $username = "root";
 $password = "";
 $dbname = "exercicio";
@@ -12,9 +12,33 @@ if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
+// consulta os dados da tabela clientes
+$sql = "SELECT id, nome, email FROM clientes";
+$result = $conn->query($sql);
 
-// Digitar PHP + SQL (1º Aqui)
+//verifica se existem registros e os exibe em formato de tabela
+if ($result->num_rows > 0){
+    echo "<h2>Lista de clientes cadastrados</h2>";
 
+    //define o formato da tabela
+    echo "<table border='2'";
+
+        //define o cabeçalho da tabela
+        echo "<tr><th>ID</th><th>Nome</th><th>Email</th></tr>";
+
+        //enquanto tiver dados preenchidos no BD
+        //listar e exibir em formato de tabela
+        while ($row = $result->fetch_assoc()){
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['nome'] . "</td>";
+            echo "<td>" . $row['email'] . "</td>";
+            echo "</tr>";
+        }
+    echo "</table>";
+}else{
+    echo "Nenhum cliente encontrado.";
+}
 
 // Fecha a conexão
 $conn->close();
